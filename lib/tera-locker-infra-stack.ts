72f591d9a52d94aa6f4941f148a2d91dbee8aa6e
Colin Bruin <colinjb23@gmail.com>
2023-03-15 14:26:05 -0700

    adding cors access to s3 bucket

--- lib/tera-locker-infra-stack.ts
+++ lib/tera-locker-infra-stack.ts
@@ -1,21 +1,30 @@
 import { Stack, StackProps, aws_s3 } from "aws-cdk-lib";
-import { BlockPublicAccess } from "aws-cdk-lib/aws-s3";
+import { BlockPublicAccess, CorsRule, HttpMethods } from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 
 export class TeraLockerInfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-    const existingBucket = aws_s3.Bucket.fromBucketArn(
-      this,
-      "ContentBucket",
-      "arn:aws:s3:::teralockerinfrastack-contentbucketlocal7b1f5302-1s1u1ljljm2gh"
-    );
-
-    console.log("existing bucket", existingBucket);
+    // const existingBucket = aws_s3.Bucket.fromBucketArn(
+    //   this,
+    //   "ContentBucket",
+    //   "arn:aws:s3:::teralockerinfrastack-contentbucketlocal7b1f5302-1s1u1ljljm2gh"
+    // );
 
     try {
       new aws_s3.Bucket(this, "ContentBucketLocal", {
         blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+        cors: [
+          {
+            allowedHeaders: ["*"],
+            allowedMethods: [
+              HttpMethods.GET,
+              HttpMethods.POST,
+              HttpMethods.PUT,
+            ],
+            allowedOrigins: ["https://localhost:8000"],
+          } as CorsRule,
+        ],
         versioned: true,
       });
     } catch (err) {
