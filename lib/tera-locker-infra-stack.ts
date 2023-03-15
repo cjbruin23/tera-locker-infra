db3caa57e9ed1a7405c6579a8730c6b821d29d57
Colin Bruin <colinjb23@gmail.com>
2023-03-15 12:38:29 -0700

    removing uneeded comment

--- lib/tera-locker-infra-stack.ts
+++ lib/tera-locker-infra-stack.ts
@@ -5,11 +5,21 @@ import { Construct } from "constructs";
 export class TeraLockerInfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+    const existingBucket = aws_s3.Bucket.fromBucketArn(
+      this,
+      "ContentBucket",
+      "arn:aws:s3:::teralockerinfrastack-contentbucketlocal7b1f5302-1s1u1ljljm2gh"
+    );
 
-    // The code that defines your stack goes here
-    new aws_s3.Bucket(this, "ContentBucket", {
-      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
-      versioned: true,
-    });
+    console.log("existing bucket", existingBucket);
+
+    try {
+      new aws_s3.Bucket(this, "ContentBucketLocal", {
+        blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+        versioned: true,
+      });
+    } catch (err) {
+      console.log("err", err);
+    }
   }
 }
